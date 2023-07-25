f1483f2d4f0e129cd986f1031cc711b0b171a692
Charlie <cnewlinds@gmail.com>
2023-07-25 12:31:42 +1000

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(name, eventName) {
+    let newArray = [];
+    for (let i =0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+        debugger;
+    }
+    return newArray;
+  
+}
+
+function countDown(number) { 
+    for (number; number >= 0; number--) {
+        console.log(number)
+    }
+}
+countDown();
